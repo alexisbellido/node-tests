3e5b4735fcc1c798156e4af9c67b5a626b7ac97d
Alexis Bellido <alexis@ventanazul.com>
2017-02-24 12:32:24 -0500

    some webpack 1 testing

--- custom-webpack/config/webpack.config.js
+++ custom-webpack/config/webpack.config.js
@@ -1,7 +1,21 @@
 // Webpack dependencies
 let path = require('path'),
-    webpack = require('webpack');
+    webpack = require('webpack'),
+    buildPath = path.resolve(__dirname, '../static');
 
 // TODO continue building webpack config
 module.exports = {
+  entry: {
+    main: [
+      './index.js'
+    ]
+  },
+  output: {
+    path: buildPath,
+    publicPath: '../',
+    filename: 'js/[name].bundle.js'
+  },
+  resolve: {
+    extensions: ['', '.js']
+  }
 }
