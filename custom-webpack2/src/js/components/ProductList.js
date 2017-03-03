22f9c7fcab426a68eb6d642e4e836599681fa2b7
Alexis Bellido <alexis@ventanazul.com>
2017-03-03 13:28:51 -0500

    play with state

--- custom-webpack2/src/js/components/ProductList.js
+++ custom-webpack2/src/js/components/ProductList.js
@@ -4,39 +4,31 @@ import sampleProducts from '../seed';
 import Product from './Product';
 
 class ProductList extends React.Component {
+    constructor (props) {
+        super(props);
+        this.state = {
+            products: []
+        };
+    }
+
+    handleProductUpvote (productId) {
+        console.log(`${productId} was upvoted`);
+    }
+
     render () {
+        // map with implicit return of Product components that is rendered inside ProductList below
         const productComponents = sampleProducts.map((product) => (
           <Product key={`product-${product.id}`}
             id={product.id}
             title={product.title}
+            votes={product.votes}
+            onVote={this.handleProductUpvote}
             description={product.description} />
         ));
         return (
             <div className="ui unstackable items">
               <h2>Product List from map in const</h2>
               {productComponents}
-              <hr />
-              <h2>Product List mapped on the fly, arrow function with implicit return</h2>
-              {
-                sampleProducts.map((product) => (
-                    <Product key={`product-${product.id}`}
-                      id={product.id}
-                      title={product.title}
-                      description={product.description} />
-                  )
-                )
-              }
-              <h2>Product List mapped on the fly</h2>
-              {
-                sampleProducts.map((product) => {
-                    return (
-                      <Product key={`product-${product.id}`}
-                        id={product.id}
-                        title={product.title}
-                        description={product.description} />
-                    );
-                })
-              }
             </div>
         );
     }
--- custom-webpack2/src/js/seed.js
+++ custom-webpack2/src/js/seed.js
@@ -5,17 +5,23 @@ module.exports = [
     {
         id: 1,
         title: 'yellow tile',
-        description: 'a good old yellow tile'
+        description: 'a good old yellow tile',
+        url: '#',
+        votes: 3
     },
     {
         id: 2,
         title: 'red monkey',
-        description: 'a monkey of a redish color'
+        description: 'a monkey of a redish color',
+        url: '#',
+        votes: 1
     },
     {
         id: 3,
         title: 'black horse',
-        description: 'this horse knows how to tango'
+        description: 'this horse knows how to tango',
+        url: '#',
+        votes: 15
     }
 ];
 
