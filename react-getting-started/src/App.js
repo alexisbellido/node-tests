9a95637e0c186cb33850b0a8cf8acfa6568e7dc6
Alexis Bellido <alexis@ventanazul.com>
2018-10-23 11:09:52 -0400

    React.createElement

--- react-getting-started/src/App.js
+++ react-getting-started/src/App.js
@@ -1,10 +1,22 @@
 import React from "react";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.headingObject = React.createElement(
+      'h3',
+      {className: 'heading'},
+      'Hi, I am a heading built with React.createElement'
+    );
+  }
+
   render() {
+    const heading = <h2 className="heading">I am a heading</h2>;
     return(
       <div className="App">
         <h1>I am a React application</h1>
+        {heading}
+        {this.headingObject}
       </div>
     );
   }
