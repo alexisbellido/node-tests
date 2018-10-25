edf9b6f5fc4a5f5f964232d92643169bec8551e5
Alexis Bellido <alexis@ventanazul.com>
2018-10-24 20:41:27 -0400

    store picker form basic styling

--- react-for-beginners/src/components/StorePicker.js
+++ react-for-beginners/src/components/StorePicker.js
@@ -3,7 +3,11 @@ import React from "react";
 class StorePicker extends React.Component {
   render() {
     return (
-      <p>Hello, I am the StorePicker</p>
+      <form className="store-selector">
+        <h2>Please enter a store</h2>
+        <input type="text" required placeholder="Store name" />
+        <button type="submit">Visit store</button>
+      </form>
     );
   } 
 }
