7092540fdf821f8c91cc00be1a2963feb391c0ff
Alexis Bellido <alexis@ventanazul.com>
2018-11-05 14:58:11 -0500

    test with React.createElement

--- react-for-beginners/src/index.js
+++ react-for-beginners/src/index.js
@@ -2,6 +2,25 @@ import React from "react";
 import ReactDOM from "react-dom";
 import StorePicker from "./components/StorePicker";
 
-// document.querySelector, which is newer, can be used instead of document.getElementById but it may be slower because it allows more selections
-ReactDOM.render(<StorePicker />, document.querySelector('#root'));
+const root = React.createElement(
+  'h1',
+  {className: 'testing'},
+  'This is ',
+  React.createElement(
+    'a',
+    {href: 'http://google.com', target: '_blank'},
+    'a link'
+  )
+);
 
+ReactDOM.render(root, document.getElementById("root"));
+
+// document.querySelector, which is newer, can be used instead of document.getElementById but it may be slower because it allows more selections
+// <React.Fragment> can be used instead of extra div to wrap sibling compoments
+// ReactDOM.render(
+//   <React.Fragment>
+//     <StorePicker />
+//     <h2>replace with some test compoment</h2>
+//   </React.Fragment>,
+//   document.querySelector('#root')
+// );
