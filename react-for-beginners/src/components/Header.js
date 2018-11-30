998783ed01cda07e71dadcc5ae17e4046c6b1043
Alexis Bellido <alexis@ventanazul.com>
2018-11-30 11:45:49 -0500

    add react-router-dom

--- react-for-beginners/src/components/Header.js
+++ react-for-beginners/src/components/Header.js
@@ -1,16 +1,39 @@
 import React from "react";
 
-class Header extends React.Component {
-  render() {
-    return (
-      <header className="top">
-        <h2>Catch of the day</h2>
-        <h3 className="tagline">
-          <span>Fresh Daily</span>
-        </h3>
-      </header>
-    );
-  }
-}
+// as there won't be state this can be a stateless functional component,
+// see how the stateful version, a class, would look below
+// function Header(props) {
+//   return (
+//     <header className="top">
+//       <h2>Catch of the day</h2>
+//       <h3 className="tagline">
+//         <span>{props.tagline}</span>
+//       </h3>
+//     </header>
+//   );
+// }
+
+// another way with an arrow function
+const Header = (props) => (
+  <header className="top">
+    <h2>The Catch of the day</h2>
+    <h3 className="tagline">
+      <span>{props.tagline}</span>
+    </h3>
+  </header>
+);
+
+// class Header extends React.Component {
+//   render() {
+//     return (
+//       <header className="top">
+//         <h2>Catch of the day</h2>
+//         <h3 className="tagline">
+//           <span>{this.props.tagline}</span>
+//         </h3>
+//       </header>
+//     );
+//   }
+// }
 
 export default Header;
