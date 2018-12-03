593f1103e1d644db8468d4988010daa6bdcf17cd
Alexis Bellido <alexis@ventanazul.com>
2018-12-03 13:07:12 -0500

    router, events and refs, binding this to custom methods, helper functions

--- react-for-beginners/src/components/StorePicker.js
+++ react-for-beginners/src/components/StorePicker.js
@@ -1,21 +1,49 @@
 import React from "react";
 // or just get { Compomnent }
 // import React, { Component } from "react";
+import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
+  constructor(props) {
+    super(props);
+    this.textInput = React.createRef();
+    // binding is necessary when using custom method goToStore
+    this.goToStore = this.goToStore.bind(this);
+  }
+
+  goToStore(event) {
+    // 1. stop form from submitting
+    event.preventDefault();
+    // 2. get text from input
+    const storeName = this.textInput.current.value;
+    // 3. change page to /store/store-name-selected using history that comes from Router.js because
+    // StorePicker is used there
+    this.props.history.push(`/store/${storeName}`);
+  }
+
+  // I can also add an arrow function as a property of the component instead of a
+  // custom method to avoid the bind in constructor to have access to this
+  // goToStore = (event) => {
+  //   event.preventDefault();
+  //   console.log(this);
+  //     console.log(this.textInput.current);
+  //     console.log(this.textInput.current.value );
+  // };
+
   render() {
     return (
       <React.Fragment>
         <h1>Form below</h1>
         { /* must use a javascript block comment here */ }
-        <form className="store-selector">
+        <form className="store-selector" onSubmit={this.goToStore}>
           <h2>Please enter a store</h2>
-          <input type="text" required placeholder="Store name" />
+          <input type="text" ref={this.textInput} required placeholder="Store name" defaultValue={getFunName()} />
           <button type="submit">Visit store</button>
         </form>
       </React.Fragment>
     );
   }
+
 }
 
 export default StorePicker;
